8b95b06c601bfb3f2fd1d51f3b80923a9a4b814e
Manoj P <manojpainam1221@gmail.com>
2026-04-03 23:16:20 +0530

    Add student ID assignment in save method of StudentRepositoryImpl

--- src/main/java/com/learninghub/learning/repository/StudentRepositoryImpl.java
+++ src/main/java/com/learninghub/learning/repository/StudentRepositoryImpl.java
@@ -9,9 +9,11 @@ import com.learninghub.learning.model.Student;
 
 @Repository
 public class StudentRepositoryImpl implements StudentRepository {
+    private int counter = 1;
 
     private final List<Student> students = new ArrayList<>();
     public void save(Student student) {
+        student.setId(counter++);
         students.add(student);
     }
 
